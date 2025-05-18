bead718f51f2a1cfe75cb1cfc7e74c274038003e
lucaunsw <lucajn101@gmail.com>
2025-05-18 21:39:16 +1000

    Build layout of pagination component

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -23,7 +23,7 @@ const IssueStatusFilter = () => {
         if (status) params.append("status", status);
         if (searchParams.get("orderBy"))
           params.append("orderBy", searchParams.get("orderBy")!);
-        searchParams.get("orderBy");
+
         const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
